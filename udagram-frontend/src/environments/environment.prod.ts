d6bbb229a11e8e6ca67c0e1ee912720634a7ffbc
Anh Nguyễn Minh <anhnm.iworking@gmail.com>
2022-11-19 21:49:00 +0700

    Anhnm - add

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9bb609a4a6f94f1ab8941bab9dab9bf-677787961.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
